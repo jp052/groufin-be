95bdff6dbc255b5c49c4bcce3c7d916d2fcde615
jp052 <jan.p2001@gmail.com>
2017-12-24 12:57:09 +1030

    Initial commit

---
+++ src/main/java/com/plankapp/groufinbe/GroufinBeApplication.java
@@ -0,0 +1,12 @@
+package com.plankapp.groufinbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroufinBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroufinBeApplication.class, args);
+	}
+}
---
+++ src/test/java/com/plankapp/groufinbe/GroufinBeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.plankapp.groufinbe;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GroufinBeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
